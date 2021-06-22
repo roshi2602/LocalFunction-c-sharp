3eaf39a3d93fe8ec2a5174df4a38c07214d531c8
roshi2602 <roshidubey2602@gmail.com>
2021-06-22 18:57:52 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//It allows you to declare a method inside the body of an already defined method. 
+//You are not allowed to use any member access modifiers in the local function
+//
+
+/* places where local functions are defined:
+
+Methods
+Constructors
+Property accessors
+Event accessor
+Lambda expressions
+Finalizers, a.k.a destructors
+*/
+namespace LocalFunction_csharp
+{
+    public class Program
+    {
+
+        public static void Main()
+        {
+            //-------------------------------------
+            //ex1
+            //local function
+            void Demo(int x, int y)
+            {
+                Console.WriteLine(x + y);
+                Console.ReadLine();
+            }
+
+            //calling local function
+            Demo(10, 20);
+
+            //------------------------------------
+            //ex2
+            //local generic functions
+
+            void Demo2<MyGeneric>(MyGeneric value)
+            {
+                Console.WriteLine(value);
+                Console.ReadLine();
+            }
+            //calling local generic function
+            Demo2<int>(123);
+            Demo2<string>("hello");
+            //-----------------------------------------
+          
+        }
+    }
+}
